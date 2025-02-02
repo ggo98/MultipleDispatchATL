343e2622fcd872dc2e78abf3ee098ed86a5bda97
ggo <ggo98@yahoo.com>
2025-02-02 05:11:22 -0800

    commit #1

--- QueryDispatchForScripts/QueryDispatchHelperXO.cpp
+++ QueryDispatchForScripts/QueryDispatchHelperXO.cpp
@@ -5,6 +5,9 @@
 
 #include <comdef.h>
 
+#include <regex>
+#include <string>
+
 // CQueryDispatchHelperXO
 
 STDMETHODIMP CQueryDispatchHelperXO::InterfaceSupportsErrorInfo(REFIID riid)
@@ -23,20 +26,100 @@ STDMETHODIMP CQueryDispatchHelperXO::InterfaceSupportsErrorInfo(REFIID riid)
 }
 
 
-STDMETHODIMP CQueryDispatchHelperXO::QueryDispatch(IDispatch* I, BSTR IIDStr, IDispatch** ret)
+static bool IsValidGUID(const std::wstring& guid) 
 {
-	_bstr_t iid_bstr_t = IIDStr;
-	int len = iid_bstr_t.length();
-	if (len > 0)
-	{
-		if (IIDStr[0] != '{'
-			&& IIDStr[len-1] != '}')
-			iid_bstr_t = L"{" + iid_bstr_t + "}";
-	}
-	IID iid;
-	HRESULT hr = IIDFromString(iid_bstr_t, &iid);
+	std::wregex guidRegex(LR"(\{?[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}\}?)", std::regex::icase);
+	return std::regex_match(guid, guidRegex);
+}
+
+static HRESULT FindInterfaceByName(IUnknown* pUnknown, const _bstr_t& interfaceName, IID& iid)
+{
+	if (!pUnknown) 
+		return E_POINTER;
+
+	// Get IProvideClassInfo
+	IProvideClassInfo* pClassInfo = NULL;
+	HRESULT hr = pUnknown->QueryInterface(IID_IProvideClassInfo, (void**)&pClassInfo);
 	if (FAILED(hr))
 		return hr;
+	IProvideClassInfoPtr classInfo;
+	classInfo.Attach(pClassInfo); // for automatic release
+	//return E_FAIL;
+
+	// Get ITypeInfo
+	ITypeInfo* pTypeInfo = NULL;
+	hr = pClassInfo->GetClassInfo(&pTypeInfo);
+	if (FAILED(hr)) 
+		return hr;
+	ITypeInfoPtr typeInfo;
+	typeInfo.Attach(pTypeInfo); // for automatic release
+
+	// Get TYPEATTR
+	TYPEATTR* pTypeAttr = nullptr;
+	hr = pTypeInfo->GetTypeAttr(&pTypeAttr);
+	if (FAILED(hr)) 
+		return hr;
+
+	bool found = false;
+	for (UINT i = 0; i < pTypeAttr->cImplTypes && !found; i++) 
+	{
+		HREFTYPE hRefType;
+		if (SUCCEEDED(pTypeInfo->GetRefTypeOfImplType(i, &hRefType))) 
+		{
+			ITypeInfo* pRefTypeInfo = NULL;
+			if (SUCCEEDED(pTypeInfo->GetRefTypeInfo(hRefType, &pRefTypeInfo))) 
+			{
+				ITypeInfoPtr refTypeInfo;
+				refTypeInfo.Attach(pRefTypeInfo); // for automatic release
+				TYPEATTR* pRefTypeAttr = NULL;
+				if (SUCCEEDED(pRefTypeInfo->GetTypeAttr(&pRefTypeAttr))) 
+				{
+					_bstr_t bstr_tName;
+					if (SUCCEEDED(pRefTypeInfo->GetDocumentation(MEMBERID_NIL, bstr_tName.GetAddress(), NULL, NULL, NULL)))
+					{
+						if (_wcsicmp(bstr_tName, interfaceName) == 0)
+						{
+							// Found matching interface name, copy IID
+							iid = pRefTypeAttr->guid;
+							found = true;
+						}
+					}
+				}
+			}
+		}
+	}
+	return found ? S_OK : E_FAIL;
+}
+
+STDMETHODIMP CQueryDispatchHelperXO::QueryDispatch(IDispatch* I, BSTR IIDStr, IDispatch** ret)
+{
+	if (!I
+		|| !IIDStr
+		|| !ret)
+		return E_POINTER;
+
+	IID iid;
+	HRESULT hr = S_OK;
+
+	_bstr_t iid_bstr_t = IIDStr;
+	if (!IsValidGUID((wchar_t*)iid_bstr_t))
+	//if (!true)
+	{
+		hr = FindInterfaceByName(I, IIDStr, iid);
+	}
+	else
+	{
+		int len = iid_bstr_t.length();
+		if (len > 0)
+		{
+			if (IIDStr[0] != '{'
+				&& IIDStr[len - 1] != '}')
+				iid_bstr_t = L"{" + iid_bstr_t + "}";
+		}
+		hr = IIDFromString(iid_bstr_t, &iid);
+		if (FAILED(hr))
+			return hr;
+	}
 	IUnknown* iu = NULL;
 	hr = I->QueryInterface(iid, (void**) &iu);
 	if (FAILED(hr))
---
+++ dotnetClient/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dotnetClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MultipleDispatchATLLib.ISimpleObject2 I = null;
+            QueryDispatchForScriptsLib.IQueryDispatchHelper QDHelper = null;
+            MultipleDispatchATLLib.ISomeOtherInterface I2 = null;
+            MultipleDispatchATLLib.ISomeOtherInterface I3 = null;
+            try
+            {
+                I = new MultipleDispatchATLLib.SimpleObjectXO();
+                MessageBox.Show(I.Method1(".NET"));
+                MessageBox.Show(I.Method2(2).ToString());
+
+                I2 = I as MultipleDispatchATLLib.ISomeOtherInterface;
+                I2.SomeOtherMethod();
+
+                QDHelper = new QueryDispatchForScriptsLib.QueryDispatchHelperXO();
+                I3 = QDHelper.QueryDispatch(I, "ISomeOtherInterface");
+                I3.SomeOtherMethod();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (null != I3)
+                    Marshal.FinalReleaseComObject(I3);
+                if (null != QDHelper)
+                    Marshal.FinalReleaseComObject(QDHelper);
+                if (null != I2)
+                    Marshal.FinalReleaseComObject(I2);
+                if (null != I)
+                    Marshal.FinalReleaseComObject(I);
+            }
+        }
+    }
+}
